4a286a6059bc6bf0299d275e47fb9cebe8a83f78
mikew <waugh.e.michael@gmail.com>
2022-02-09 14:56:05 -0500

    multiple dispatch -> finc integration

--- src/ha-trade-solution.jl
+++ src/ha-trade-solution.jl
@@ -1,5 +1,14 @@
-function ha_trade_equilibrium(W, τ_revenue, R, model_params, trade_params)
+# Note how multiple dispatch is being used here...
+
+##########################################################################
+##########################################################################
+
+function ha_trade_equilibrium(W::Array{T}, τ_revenue::Array{T}, R::Array{T}, model_params, trade_params) where T
     # Combines the demand side with the trade side.
+    #
+    # Using multiple dispatch here to determine the level of finacial integration.
+    # If type of R is array -> each country has it's own rate -> in fincial autarky, so the bond market
+    # needs to clear within the country
 
     @unpack Ncntry, A = trade_params
 
@@ -33,12 +42,68 @@ function ha_trade_equilibrium(W, τ_revenue, R, model_params, trade_params)
 
     AD = [output_stats[cnt].AD[1] for cnt in 1:Ncntry]
 
-    N = [output_stats[cnt].N[1] for cnt in 1:Ncntry]
+    Nf = [output_stats[cnt].N[1] for cnt in 1:Ncntry]
 
     asset_net_demand = [output_stats[cnt].Aprime[1] for cnt in 1:Ncntry]
     # then this is the asset market clearing condition
 
-    trade_net_demand = trade_equilibrium(W, AD, N, τ_revenue, trade_params)
+    trade_net_demand = trade_equilibrium(W, AD, Nf, τ_revenue, trade_params)
+
+    return [ trade_net_demand ; asset_net_demand ]
+
+end
+
+##########################################################################
+##########################################################################
+
+function ha_trade_equilibrium(W::Array{T}, τ_revenue::Array{T}, R::T, model_params, trade_params) where T
+    # Combines the demand side with the trade side.
+    #
+    # Using multiple dispatch here to determine the level of finacial integration.
+    # If type of R is singlton of type T -> there is world interest rate for which the bond market
+    # needs to clear at that one rate. 
+
+    # I think not hard typing, using T helps if autodiff is employed
+
+    @unpack Ncntry, A = trade_params
+
+    output_stats = Array{NIPA{eltype(W)}}(undef, Ncntry)
+    hh = household{eltype(W)}
+    dist = distribution{eltype(W)}
+
+    Pces = goods_prices(W, trade_params)[2]
+
+    
+    # Given the wage, we need to know the price index to solve the 
+    # hh problem. So do this first.
+
+    for xxx = 1:Ncntry  # do this for each country.
+                        # this is the place to use distributed or pmap
+                        # hh problems can be solved independtly
+
+        hh, dist = compute_eq(Pces[xxx], W[xxx], τ_revenue[xxx], R,  model_params);
+        # This is the BHA part. Given prices, solve hh problem and construct stationary distribution L
+        # internaltionally all the matters from the hh perspective is the price index.
+
+        output_stats[xxx] = aggregate(Pces[xxx], W[xxx], τ_revenue[xxx], R,
+                                     hh, dist, A[xxx], params)
+
+    end
+    
+    # We have each countries total demand of goods: AD
+    # And each countries total supply of goods: A*N
+    
+    # Pass supply and demand to the trade block and see if they match up.
+
+    AD = [output_stats[cnt].AD[1] for cnt in 1:Ncntry]
+
+    Nf = [output_stats[cnt].N[1] for cnt in 1:Ncntry]
+
+    asset_net_demand = sum([output_stats[cnt].Aprime[1] for cnt in 1:Ncntry])
+    # then this is the asset market clearing condition
+    # sum across world asset demand---given how mulitiple dispatch is working
+
+    trade_net_demand = trade_equilibrium(W, AD, Nf, τ_revenue, trade_params)
 
     return [ trade_net_demand ; asset_net_demand ]
 
