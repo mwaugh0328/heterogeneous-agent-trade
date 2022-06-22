853448e3e1ac3a46d7a6176e9ab6b984ea174c41
mikew <waugh.e.michael@gmail.com>
2022-06-22 11:14:07 -0400

    ek data

--- code/julia/scratch-pad-market-clear.jl
+++ code/julia/scratch-pad-market-clear.jl
@@ -3,14 +3,23 @@ include("ha-trade-solution.jl")
 include("ha-trade-helper-functions.jl")
 include("static-trade-environment.jl")
 using MINPACK
+using MAT
 
-Ncntry = 4
+file = matopen("../../ek-data/ek-output.mat")
+tradeshare = read(file, "tssdmat")
 
-dtest = 2.0
-d = dtest.*ones(Ncntry,Ncntry)
-d[diagind(d)] .= 1.0
+d = read(file, "rtausd")
 
-TFP = [1.0, 1.0, 0.75, 1.25]
+Ncntry = size(d)[1]
+
+#TFP = vec(exp.(read(file, "ssd")))
+
+
+# dtest = 2.0
+# d = dtest.*ones(Ncntry,Ncntry)
+# d[diagind(d)] .= 1.0
+
+TFP = ones(Ncntry)
 
 mdl_prm = world_model_params(Ncntry = Ncntry, Na = 100, Nshocks = 5, 
 γ = 2.0, ϕ = 3, amax = 8.0, σ = 0.40, ρ = 0.20, σϵ = 0.25, d = d, TFP = TFP)
@@ -18,7 +27,7 @@ mdl_prm = world_model_params(Ncntry = Ncntry, Na = 100, Nshocks = 5,
 @unpack Na, Nshocks, Ncntry, TFP = mdl_prm
 
 R = 1.03*ones(Ncntry);
-W = 1.0*ones(Ncntry);
+W = TFP;
 
 f(x) = world_equillibrium(x, mdl_prm, hh_solution_method = "itteration");
 
@@ -30,19 +39,20 @@ end
 
 initial_x = [W[2:end]; R]
 
-n = length(initial_x)
-diag_adjust = n - 1
+# n = length(initial_x)
+# diag_adjust = n - 1
 
-sol = fsolve(f!, initial_x, show_trace = true, method = :hybr;
-      ml=diag_adjust, mu=diag_adjust,
-      diag=ones(n),
-      mode= 1,
-      tol=1e-5,
-       )
+# sol = fsolve(f!, initial_x, show_trace = true, method = :hybr;
+#       ml=diag_adjust, mu=diag_adjust,
+#       diag=ones(n),
+#       mode= 1,
+#       tol=1e-5,
+#        )
 
-print(sol)
+# print(sol)
 
-Wsol = [1.0; sol.x[1:Ncntry-1]]
-Rsol = sol.x[Ncntry:end]
+# Wsol = [1.0; sol.x[1:Ncntry-1]]
+# Rsol = sol.x[Ncntry:end]
 
-Y, tradeflows, A_demand, tradeshare, hh, dist = world_equillibrium(Rsol, Wsol, mdl_prm, hh_solution_method = "itteration");
+# Y, tradeflows, A_demand, tradeshare, hh, dist = world_equillibrium(Rsol, Wsol, 
+#     mdl_prm, hh_solution_method = "itteration");
