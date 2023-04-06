6af0d44aaa7000526c527abbfb25f282a10e46b2
mikew <waugh.e.michael@gmail.com>
2023-04-05 21:42:43 -0400

    two country notebook

--- code/julia/ha-trade-elasticity.jl
+++ code/julia/ha-trade-elasticity.jl
@@ -1,8 +1,8 @@
 struct θs{T}
-    θπ::Array{T} 
-    θc::Array{T} 
-    θπii::Array{T} 
-    θcii::Array{T}
+    θπ::Array{T} # how πij changes
+    θc::Array{T}  # how cij changes
+    θπii::Array{T} # how πii changes w.r. dij
+    θcii::Array{T} # how cii changes w.r. dij
 end
 
 ##########################################################################
@@ -56,6 +56,7 @@ end
 ##########################################################################
 
 function make_θ(household, homecontry, R, W, p, model_params; points = 3, order = 1)
+    # makes the micro-level elasticities
 
     @unpack cons_policy, Tv = household
 
@@ -66,12 +67,13 @@ function make_θ(household, homecontry, R, W, p, model_params; points = 3, order
 
     @inbounds for idxj = 1:model_params.Ncntry
 
-        # first construct the intensive margin elascitity
+        # now construct the extensive margin elascitity
 
         h(x) = make_θπ(x, homecontry, idxj, p, cons_policy, Tv, R, W, model_params)
         
         foobar = central_fdm(points, order, adapt = 0)(h, log.(p[idxj]))
 
+        # this then upacks things to grab both ∂πij / ∂dij and ∂πii / ∂dij
         θπ[:,:,idxj] .= foobar[1:model_params.Na,:] 
 
         θπii[:,:,idxj] .= foobar[model_params.Na+1:end,:] 
@@ -80,12 +82,13 @@ function make_θ(household, homecontry, R, W, p, model_params; points = 3, order
 
     @inbounds for idxj = 1:model_params.Ncntry
 
-        # first construct the intensive margin elascitity
+        # now construct the intensive margin elascitity
 
         h(x) = make_θc(x, homecontry, idxj, p, cons_policy, Tv, R, W, model_params)
         
         foobar = central_fdm(points, order, adapt = 0)(h, log.(p[idxj]))
 
+        # this then upacks things to grab both ∂cij / ∂dij and ∂cii / ∂dij
         θc[:,:,idxj] .= foobar[1:model_params.Na,:] 
 
         θcii[:,:,idxj] .= foobar[model_params.Na+1:end,:] 
@@ -116,12 +119,11 @@ function make_θπ(logp, homecntry, idxj, pvec, gc, v, R, W, model_params)
     # (ii) and ∂V/∂a (which should be zero via envelope theorem)
     # no change from future shift in shares
 
-    #∂shares then computes **only** numerator of share, so the assumption
-    #here is denominator is fixed, this is mc assumption
-
     πprob = make_πprob(Tv, model_params.σϵ)
     
     return vcat(log.( πprob[:,:,idxj] ), log.( πprob[:,:,homecntry] ))
+    # when passed through numerical diff it returns
+    # ∂πij / ∂dij and ∂πii / ∂dij
     
 end
 
@@ -140,5 +142,6 @@ function make_θc(logp, homecntry, idxj, pvec, gc, v, R, W, model_params)
     Kgc .= coleman_operator(gc, v, R, W, foo, model_params)[1]
     
     return vcat(log.( Kgc[:,:,idxj] ), log.( Kgc[:,:,homecntry] ))
+    # this then upacks things to grab both ∂cij / ∂dij and ∂cii / ∂dij
     
 end
