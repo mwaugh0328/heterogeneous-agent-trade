2c3d43bba46a8deb68c54bd3c1983acf522ab691
Thomas Hasenzagl <thomas.hasenzagl@gmail.com>
2023-06-07 10:20:14 -0500

    two coutnry python code matches up julia

--- code/python/hat_solution.py
+++ code/python/hat_solution.py
@@ -1,48 +1,49 @@
 import numpy as np
 import scipy as sp
+from copy import copy
 from hat_support import *
 
 class HATmodel:
 
-    def __init__(self, β, σ, α, φ, δ, σ_ε, N, P, L):
-        self.β, self.σ, self.α, self.φ = β, σ, α, φ
-        self.δ, self.σ_ε, self.N, self.P = δ, σ_ε, N, P
-        self.L = L
+    def __init__(self, β, γ, φ, σ_ε, N, P, L, TFP, τ, d, agrid, zgrid):
+        self.β, self.γ, self.φ = β, γ, φ
+        self.σ_ε, self.N, self.P = σ_ε, N, P
+        self.L, self.TFP, self.τ, self.d = L, TFP, τ, d 
+        self.agrid, self.zgrid = agrid, zgrid
 
     def utility(self, c):
-        σ = self.σ
-        return ((1-σ)**(-1))*(c**(1-σ))
+        γ = self.γ
+        return ((1-γ)**(-1))*(c**(1-γ))
 
     def u_c(self, c):
-        σ = self.σ        
-        return c**(-σ)
+        γ = self.γ        
+        return c**(-γ)
 
     def u_c_inv(self, x):
-        σ = self.σ        
-        
-        return x**(-1/σ) 
+        γ = self.γ        
+        return x**(-1/γ) 
 
-    def find_w(self, R, TFP):
-        α, δ = self.α, self.δ
-        return TFP*(1-α)*((R-1+δ)/(TFP*α))**(α/(α-1)) 
+    def find_p(self, w):
+        TFP, d, τ = self.TFP, self.d, self.τ
+        return (w / TFP) * d * (1 + τ)
 
-    def find_K(self, R, TFP, N):
-        α, δ = self.α, self.δ
-        return N*((R-1+δ)/(TFP*α))**(1/(α-1)) 
+    def find_Y(self):
+        TFP, L = self.TFP, self.L
+        return TFP*L
 
-    def find_Y(self, K, TFP, N):
-        α = self.α
-        return TFP*(K**α)*(N**(1-α)) 
+    def change_d(self, d):
+        self.d = d
 
-    def find_X(self, K):
-        δ = self.δ 
-        return δ*K 
-
-    def coleman(self, agrid, zgrid, x, R, w, p):
+    def coleman(self, x, R, w):
         u_c, u_c_inv = self.u_c, self.u_c_inv
-        β, φ, P, N = self.β, self.φ, self.P, self.N 
+        β, φ, P, N = self.β, self.φ, self.P, self.N
+        agrid, zgrid = self.agrid, self.zgrid
         find_value_function = self.find_value_function
         find_probabilities = self.find_probabilities
+        find_p = self.find_p
+
+        # Find prices
+        p = find_p(w)
 
         # Unpack policy and value function
         gc = x[0:agrid.shape[0], :, :]
@@ -75,14 +76,15 @@ class HATmodel:
         cih = zgrid*w + R*agrid[:,np.newaxis]
         gc1 = (cih[:,:,np.newaxis] - ga1)/p        
 
-        v1 = find_value_function(agrid, zgrid, v, gc1, ga1)
+        v1 = find_value_function(v, gc1, ga1)
         x1 = np.concatenate([gc1, v1])
 
         return x1 
 
-    def find_value_function(self, agrid, zgrid, v, gc, ga):
+    def find_value_function(self, v, gc, ga):
         find_log_sum, utility = self.find_log_sum, self.utility
         P, N = self.P, self.N
+        agrid, zgrid = self.agrid, self.zgrid
         β = self.β
 
         for N_i in range(N):
@@ -96,6 +98,7 @@ class HATmodel:
                     elif ga[a_i, z_i, N_i] >= agrid[-1]:
                         Ev += P[z_i, :] @ find_log_sum(v[agrid.shape[0]-1, :, :], zgrid)
                     else: 
+                        
                         idx = np.where(ga[a_i, z_i, N_i]>=agrid)[0][-1]
                         q = ((ga[a_i, z_i, N_i] - agrid[idx]) / (agrid[idx+1] - agrid[idx]))
                         Ev += (1-q) * P[z_i, :] @ find_log_sum(v[idx, :, :], zgrid)
@@ -105,20 +108,25 @@ class HATmodel:
 
         return v
 
-    def find_policies(self, agrid, zgrid, R, w, p):
+    def find_policies(self, R, w):
         u_c, u_c_inv = self.u_c, self.u_c_inv
         coleman, find_probabilities = self.coleman, self.find_probabilities      
-        φ, β, N = self.φ, self.β, self.N 
+        φ, β, N = self.φ, self.β, self.N
+        agrid, zgrid = self.agrid, self.zgrid
         fixed_point_iteration = self.fixed_point_iteration
+        find_p = self.find_p
+
+        # Find prices
+        p = find_p(w)
 
         # Initial guess for consumption policy
-        gc0_vec = np.linspace(0.1, 3, agrid.shape[0])
+        gc0_vec = np.linspace(0.1, agrid[-1], agrid.shape[0])
         gc0 = np.repeat(np.repeat(gc0_vec[:,np.newaxis], zgrid.shape[0], axis=1)[:,:,np.newaxis], N, axis=2) 
         v0 = (-1)*np.ones((agrid.shape[0], zgrid.shape[0], N))/(1-β)
         x0 = np.concatenate([gc0, v0])
 
         # Find fixed point of coleman operator
-        fun = lambda x: coleman(agrid, zgrid, x, R, w, p)     
+        fun = lambda x: coleman(x, R, w)     
         x = fixed_point_iteration(fun, x0, agrid)
 
         gc = x[0:agrid.shape[0], :, :]
@@ -141,7 +149,7 @@ class HATmodel:
             x1 = fun(x)
             gc1 = x1[0:agrid.shape[0], :, :]
 
-            if np.abs(np.max(gc1-gc)) < error:
+            if np.max(np.abs(gc1-gc)) < error:
                 return x1
             else:
                 x = x1
@@ -163,11 +171,12 @@ class HATmodel:
 
         return log_sum
 
-    def find_transition_matrix(self, agrid, zgrid, π, ga):
+    def find_transition_matrix(self, π, ga):
         """
         Transition matrix for the vector s = [(a1,z1),...,(aM,z1),(a1,z2),...,(aM,z2),...(aM,zN)]'        
         """
         P, N = self.P, self.N
+        agrid, zgrid = self.agrid, self.zgrid
 
         # Find the transition matrix
         T = np.zeros((agrid.size*zgrid.size, agrid.size*zgrid.size))
@@ -208,33 +217,183 @@ class HATmodel:
 
         return λ/λ.sum()
 
-    def find_aggregate_var(self, g, agrid, zgrid, λ):
-
-        return np.dot(λ, g.reshape(-1, order='F'))
-
-    def find_equilibrium(self, agrid, zgrid, R, w, p):
+    def solve_model(self, R, w, cntry_idx=0):
         find_policies = self.find_policies
         find_transition_matrix = self.find_transition_matrix
         find_stationary_distribution = self.find_stationary_distribution
-        find_aggregate_var = self.find_aggregate_var
-        N = self.N
+        agrid, zgrid = self.agrid, self.zgrid
+        find_p, find_aggregates = self.find_p, self.find_aggregates
+
+        # Find places
+        p = find_p(w)
 
         # Find policies
-        gc, ga, π = find_policies(agrid, zgrid, R, w, p)
+        gc, ga, π = find_policies(R, w)
 
         # Find transition matrix
-        T = find_transition_matrix(agrid, zgrid, π, ga)
+        T = find_transition_matrix(π, ga)
 
         # Find stationary distribution
         λ = find_stationary_distribution(T)
-        λ_a = np.sum(λ.reshape([agrid.size, zgrid.size], order='F'),1)
 
-        return AiyagariEquilibrium(gc, ga, π, T, λ, λ_a)
+        # Find aggregate quantities
+        A_i, Y_i, C_i, C_ij, G_i = find_aggregates(gc, ga, π, p, λ, cntry_idx)
 
-class AiyagariEquilibrium:
+        return A_i, Y_i, C_i, C_ij, G_i
 
-    def __init__(self, gc, ga, π, T, λ, λ_a):
-        self.gc, self.ga, self.π = gc, ga, π
-        self.T, self.λ, self.λ_a = T, λ, λ_a
+    def find_aggregates(self, gc, ga, π, p, λ, cntry_idx):
+        agrid, zgrid = self.agrid, self.zgrid
+        TFP, L, d, τ = self.TFP, self.L, self.d, self.τ
+        N = self.N
+
+        # Compute aggregate assets
+        λ_a = np.sum(λ.reshape([agrid.size, zgrid.size], order='F'), 1)
+        A = λ_a @ agrid 
+
+        # Output
+        λ_e = np.sum(λ.reshape([agrid.size, zgrid.size], order='F'), 0)
+        Y = p[cntry_idx] * TFP * L * λ_e @ zgrid
+
+        # Total Consumption and consumption by country
+        C = L * np.reshape(np.sum(p * gc * π, 2), -1, order='F') @ λ 
+        C_j = L *  np.reshape(p * gc * π, (agrid.shape[0]*zgrid.shape[0], N), order='F').T @ λ
+
+        # Government
+        G = L * τ 
+
+        return A, Y, C, C_j, G
+
+
+class MulticountryHATmodel:
+
+    def __init__(self, β, γ, φ, σ_ε, N, P, L, TFP, τ, d, agrid, zgrid, numeraire_cntry = 0):
+        self.β, self.γ, self.φ = β, γ, φ
+        self.σ_ε, self.N, self.P = σ_ε, N, P
+        self.L, self.TFP, self.τ, self.d = L, TFP, τ, d
+        self.τ, self.d = τ, d
+        self.agrid, self.zgrid = agrid, zgrid
+        self.numeraire_cntry = numeraire_cntry 
+
+    def solve_model_FG(self, R, w):
+        β, γ, φ, σ_ε = self.β, self.γ, self.φ, self.σ_ε
+        N, P, L, TFP = self.N, self.P, self.L, self.TFP
+        τ, d = self.τ, self.d
+        agrid, zgrid = self.agrid, self.zgrid
+        find_trade_stats = self.find_trade_stats 
+
+        A_i = np.empty(N)
+        Y_i = np.empty(N)
+        C_i = np.empty(N)
+        G_i = np.empty(N)
+        C_ij = np.empty((N, N))
+        for i in range(N):
+            m_i = HATmodel(β, γ, φ, σ_ε, N, P, L[i], TFP[i], τ[i], d[i,:], agrid, zgrid)    
+            A_i[i], Y_i[i], C_i[i], C_ij[i, :], G_i[i] = m_i.solve_model(R, w[i], i)
+
+        # Find total exports and imports
+        M_i, X_i, tradeshares = find_trade_stats(C_ij)
+
+        return A_i, Y_i, C_i, C_ij, G_i, M_i, X_i, tradeshares
+
+    def objective_FG(self, x):
+        numeraire_cntry = self.numeraire_cntry
+        solve_model_FG = self.solve_model_FG
+        N = self.N
+
+        x = np.exp(x)
+        R = x[0]
+        w = np.zeros(N)
+        mask = np.zeros(N, dtype=bool)
+        mask[numeraire_cntry] = True
+        w[mask] = 1
+        w[mask==False] = x[1:]       
+
+        A_i, Y_i, C_i, C_ij, G_i, M_i, X_i, tradeshares = solve_model_FG(R, w)
+        Y_i1 = C_i + G_i + X_i - M_i 
+
+        resid = np.zeros(N)
+        resid[0] = A_i.sum()
+        resid[1:] = Y_i[mask==False] - Y_i1[mask==False] 
+
+        return resid
+
+    def find_trade_stats(self, C_ij):
+        N = self.N
+
+        X_i = np.empty(N)
+        M_i = np.empty(N)
+        tradeshares = np.empty((N, N))
+        for i in range(N):
+            M_i[i] = C_ij[i, :].sum() - C_ij[i, i]
+            X_i[i] = C_ij[:, i].sum() - C_ij[i, i]        
+            tradeshares[i,:] = C_ij[i, :]/C_ij[i, :].sum() 
+
+        return M_i, X_i, tradeshares
+
+    def find_equilibrium_FG(self, x0):
+        objective_FG = self.objective_FG 
+        solve_model_FG = self.solve_model_FG
+        numeraire_cntry = self.numeraire_cntry
+        N = self.N  
+
+        out = sp.optimize.root(objective_FG, np.log(x0))
+        out = np.exp(out.x)
+
+        R = out[0]
+        w = np.zeros(N)
+        mask = np.zeros(N, dtype=bool)
+        mask[numeraire_cntry] = True
+        w[mask] = 1
+        w[mask==False] = out[1:]    
+
+        A_i, Y_i, C_i, C_ij, G_i, M_i, X_i, tradeshares = solve_model_FG(R, w) 
+
+        return Equilibrium(R, w, A_i, Y_i, C_i, C_ij, G_i, M_i, X_i, tradeshares) 
+
+    def find_log_c(self, m_i, R, w, log_d):
+
+        # Change d
+        m_i1 = copy(m_i)
+        m_i1.change_d(np.exp(log_d))
+
+        # Find policies
+        gc, ga, π = m_i1.find_policies(R, w)
+
+        return np.log(gc), np.log(π) 
+
+    def find_trade_elasticities(self, R, w, i, h=1e-8):
+        β, γ, φ, σ_ε = self.β, self.γ, self.φ, self.σ_ε
+        N, P, L, TFP = self.N, self.P, self.L, self.TFP
+        τ, d, N = self.τ, self.d, self.N
+        agrid, zgrid = self.agrid, self.zgrid
+        find_log_c = self.find_log_c
+
+        # Model instance for country i
+        m_i = HATmodel(β, γ, φ, σ_ε, N, P, L[i], TFP[i], τ[i], d[i,:], agrid, zgrid)    
+
+        # Function we want to take the derivative of
+        fn = lambda log_d: find_log_c(m_i, R, w[i], log_d)
+
+        θc = np.zeros((agrid.shape[0], zgrid.shape[0], N))
+        θπ = np.zeros((agrid.shape[0], zgrid.shape[0], N))
+        for j in range(N):
+            hvec = np.zeros(N)
+            hvec[j] = h
+            
+            gc1, π1 = fn(np.log(d[i,:]) + hvec) 
+            gc2, π2 = fn(np.log(d[i,:])) 
+
+            θc[:,:,j] = h**(-1)*(gc1[:,:,j] - gc2[:,:,j])
+            θπ[:,:,j] = h**(-1)*(π1[:,:,j] - π2[:,:,j]) 
+
+        return θc, θπ 
+
+class Equilibrium:
+
+    def __init__(self, R, w, A_i, Y_i, C_i, C_ij, G_i, M_i, X_i, tradeshares):
+        self.R, self.w = R, w
+        self.A_i, self.Y_i, self.C_i = A_i, Y_i, C_i 
+        self.C_ij, self.G_i, self.M_i = C_ij, G_i, M_i 
+        self.X_i, self.tradeshares = X_i, tradeshares 
 
 
--- code/python/hat_support.py
+++ code/python/hat_support.py
@@ -1,6 +1,16 @@
 import numpy as np
+import scipy as sp
 from scipy.stats import norm
 from numba import jit
+
+def expanding_grid(a, b, θ, n):
+    grid = np.linspace(0, 1, n)
+
+    grid_e = np.zeros(n)
+    for x_i, x in enumerate(grid): 
+        grid_e[x_i] = a + (b-a)*x**θ
+
+    return grid_e
 
 def tauchen(n, ρ, σ, m=3, tol=1e-8):
         """
@@ -57,13 +67,82 @@ def tauchen(n, ρ, σ, m=3, tol=1e-8):
 
         return z, P, zstar 
 
-def expanding_grid(a, b, θ, n):
-    grid = np.linspace(0, 1, n)
+def rouwenhorst(n, ρ, σ, μ = 0.0):
+    σ_y = σ / (1 - ρ**2)**0.5
+    p = (1 + ρ)*0.5
+    Θ = np.array([[p, 1 - p], [1 - p, p]])
+    ψ = ((n - 1)**0.5) * σ_y
+    m = μ / (1 - ρ)
+    z, P = _rouwenhorst(p, p, m, ψ, n)
 
-    grid_e = np.zeros(n)
-    for x_i, x in enumerate(grid): 
-        grid_e[x_i] = a + (b-a)*x**θ
+    return z, P 
 
-    return grid_e
+def _rouwenhorst(p, q, m, Δ, n):
+    if n == 2:
+        return np.array([m - Δ, m + Δ]),  np.array([[p, 1 - p], [1 - q, q]])
+    else:
+        _, θ_nm1 = _rouwenhorst(p, q, m, Δ, n - 1)
 
+        θn = (p * np.append(np.append(θ_nm1, np.zeros((n-1, 1)), 1), np.zeros((1, n)), 0) +
+             (1 - p) * np.append(np.append(np.zeros((n-1, 1)), θ_nm1, 1), np.zeros((1, n)), 0) +
+             q * np.append(np.zeros((1, n)), np.append(np.zeros((n-1, 1)), θ_nm1, 1), 0) +
+             (1 - q) * np.append(np.zeros((1, n)), np.append(θ_nm1, np.zeros((n-1, 1)), 1), 0)) 
 
+        θn[1:-1, :] /= 2
+
+        return np.linspace(m - Δ, m + Δ, n), θn
+
+def markov_chain(n_p, n_iid, ρ_p, σ_p, σ_iid, w0=1, tol=1e-8):
+    # adapted from Manuel Amador's julia code
+    # numbers from KMP
+    # ar1 = 0.9695
+    # sigmaP = sqrt(0.0384)/(1.2)
+    # sigmaIID = sqrt(0.0522)/(1.2)
+    # P, z_vals = calibration(5, 2 , ar1, sigmaP, sigmaIID)
+
+    # persistent component
+    y_vals, P_p = rouwenhorst(n_p, ρ_p, σ_p)
+    y_vals = np.exp(y_vals)
+
+    # iid component
+    if n_iid > 1:
+        x_vals, P_iid = rouwenhorst(n_iid, 0.0, σ_iid)
+        x_vals = np.exp(x_vals)
+    else:
+        P_idd = 1.0
+        x_vals = 1.0
+
+    # Initialize arrays
+    n_z = n_p * n_iid
+    z_vals = np.empty(n_z) 
+    P = np.empty((n_z, n_z))
+
+    idx = 0
+    for i in range(n_p):
+        for j in range(n_iid):
+            z_vals[idx] = y_vals[i] * x_vals[j]
+            idx += 1
+
+    idx = 0
+    for i in range(n_p):
+        for j in range(n_iid):
+            idx2 = 0
+            for k in range(n_p):
+                for l in range(n_iid):
+                    P[idx,idx2] = P_p[i,k] * P_iid[j,l]
+                    idx2 += 1
+            idx += 1
+
+    # Find stationary distibution and demean
+    # Find eigenvalue closest to 1
+    w, v = sp.sparse.linalg.eigs(P.T, k=1, sigma=1)
+
+    # Find stationary distribution
+    zstar = np.real(v.flatten())
+    zstar /= zstar.sum()    
+
+    # Find ergodic mean
+    z_mean = z_vals @ zstar 
+    z_vals = (w0/z_mean) * z_vals
+
+    return np.log(z_vals), P
