262ab51be9aea11a64f03d579aac52323530d850
Teerat Wongrattanapiboon <wongr003@umn.edu>
2024-08-06 15:16:17 -0500

    delete old functions

--- code/julia/ha-trade-transition.jl
+++ code/julia/ha-trade-transition.jl
@@ -1,3 +1,19 @@
+function push_foward!(λ, Q, household, model_params)
+    # Pushes the economy forward
+    # (1) Take policy functions and a distribution L -> aggregates today.
+    # Policy functions -> Transition probability Q
+    # Distribution today + Q -> Distribution tomorrow. 
+    make_Q!(Q, household, model_params)
+
+    # this had an alt_make_Q, not sure what difference is, more complicated
+    # 
+    
+    # Then push the distribution forward
+    λ = law_of_motion(λ , transpose(Q))
+    
+end
+
+#####################################################################################################
 
 function one_step_itteration(cₜ₊₁, vₜ₊₁, Rₜ, Rₜ₊₁, Wₜ, pₜ, pₜ₊₁, τ, model_params)
     # used to work backward. Give me a policy function and V at date
